672fb014404744b37aa5453c267aed0e72962c57
Michael Kettel <mkettel@gmail.com>
2024-04-26 13:27:48 +0200

    fix typo3 v12 issues

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -6,4 +6,12 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die();
 
-ExtensionManagementUtility::addStaticFile('rms_extlocate_extend', 'Configuration/TypoScript', 'rms_extlocate_extend');
+call_user_func(
+    static function () {
+        ExtensionManagementUtility::addStaticFile(
+            'rms_extlocate_extend',
+            'Configuration/TypoScript',
+            'rms_extlocate_extend'
+        );
+    }
+);
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-defined('TYPO3') || die();
+defined('TYPO3') || die('Access denied.');
 
 (static function () {
     $_EXTKEY = 'rms_extlocate_extend';
